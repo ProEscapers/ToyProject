86bc3b193548726bf8c77d5dab3a2605c38db2ab
younglaecho <tks7205@gmail.com>
2022-10-26 22:49:24 +0900

    development environment setting

---
+++ ToyProject/core/src/main/java/asd/coreApplication.java
@@ -0,0 +1,14 @@
+package asd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class coreApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(coreApplication.class, args);
+    }
+
+}
